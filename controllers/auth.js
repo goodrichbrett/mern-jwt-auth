c0b3acef6e98276862c73067ca2ced14c30bfdf2
DavidStinson <mail@davidstinson.dev>
2020-09-14 12:52:39 -0500

    start to persisit token on clinet

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   signup
@@ -8,9 +9,21 @@ async function signup(req, res) {
   const user = new User(req.body);
   try {
     await user.save();
-    // TODO: Send back a JWT instead of the user
-    res.json(user);
+    // Be sure to first delete data that should not be in the token
+    const token = createJWT(user);
+    res.json({ token });
   } catch (err) {
     res.status(400).send({'err': err.errmsg});
   }
+}
+
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+  return jwt.sign(
+    { user }, // data payload
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
 }
